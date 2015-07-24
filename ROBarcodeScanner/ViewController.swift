c70cf3ec648ce3ed439cf6f8ae01f7bfda37c40c
Robin Oster <robin.oster@rascor.com>
2015-07-24 10:50:54 +0200

    Initial Commit

---
+++ ROBarcodeScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ROBarcodeScanner
+//
+//  Created by Robin Oster on 24/07/15.
+//  Copyright (c) 2015 RASCOR International AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
